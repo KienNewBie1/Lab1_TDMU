38011a434310241783a15bc06e079a98d1a3c8cf
KienNewBie1 <trungkien98744@gmail.com>
2023-09-18 16:02:48 +0700

    add all

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// GetChuDe
+        /// <returns>ReturnChuDe</returns>
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+    }
+}
